78225a3d5e598a5a9bf3ed13c717df46eb60173c
peih68 <hiepxuxi6688@gmail.com>
2025-05-23 11:49:23 +0700

    add frontend base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-blue-100">
+            <h1 className="text-4xl font-bold text-blue-700">QAirline Booking App ✈️</h1>
+        </div>
+    );
+}
+
+export default App;
