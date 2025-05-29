80dea979b11dbf7a5211807fad972bd5632c74ca
peih68 <hiepxuxi6688@gmail.com>
2025-05-29 11:14:07 +0700

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,50 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Footer, Navbar } from "./components";
+import {
+    Confirm,
+    FlightExplore,
+    Flights,
+    Hotels,
+    Packages,
+    PassengerInfo,
+    Payment,
+    SeatSelect,
+} from "./pages";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-blue-100">
-            <h1 className="text-4xl font-bold text-blue-700">QAirline Booking App ✈️</h1>
-        </div>
+        <>
+            <div className="font-Nunito overflow-hidden max-w-[1440px] mx-auto">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Flights />} />
+                    <Route path="/hotels" element={<Hotels />} />
+                    <Route path="/packages" element={<Packages />} />
+                    <Route path="/explore" element={<FlightExplore />} />
+                    <Route path="/passenger-info" element={<PassengerInfo />} />
+                    <Route path="/seat-selection" element={<SeatSelect />} />
+                    <Route path="/payment" element={<Payment />} />
+                    <Route path="/confirm" element={<Confirm />} />
+                </Routes>
+                <Footer />
+                <ToastContainer
+                    position="bottom-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="dark"
+                />
+            </div>
+        </>
     );
-}
+};
 
 export default App;
