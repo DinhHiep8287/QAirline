8ea523f65adf94a0baf22078c4f7f61329d9fbfc
tr1la <phucuongto2004@gmail.com>
2025-05-30 11:40:49 +0700

    Fix signup link

--- frontend/src/container/Signup.jsx
+++ frontend/src/container/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = ({ signup, setSignup, setSignin }) => {
         }
 
         try {
-            const response = await axios.post("http://localhost:8080/api/v1/auth/register", {
+            const response = await axios.post("http://localhost:8080/api/v1/auth/signup", {
                 email,
                 password
             });
